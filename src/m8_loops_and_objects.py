8ecb673c4d2728d6a180ff5a969aad6324904a25
568289315 <wangl3@rose-hulman.edu>
2017-09-06 17:08:48 -0400

    Finish TODO

--- src/m8_loops_and_objects.py
+++ src/m8_loops_and_objects.py
@@ -10,6 +10,7 @@ Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
 """  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
+import math
 
 
 def main():
@@ -20,7 +21,8 @@ def main():
     print_sequence2()
     draw_circles2()
     print_sequence3()
-
+    draw_circles3()
+    print_cosines()
 
 def print_sequence1():
     """
@@ -171,7 +173,7 @@ def draw_circles3():
     -- Waits for the user to press the mouse, then closes the window.
     """
     # ------------------------------------------------------------------
-    # TODO: 7. Implement this function, per its doc-string above.
+    # DONE: 7. Implement this function, per its doc-string above.
     # Put a statement in  main  to test this function.
     # REQUIREMENT: You must use a   RANGE  statement to solve this problem.
     # ------------------------------------------------------------------
@@ -179,6 +181,14 @@ def draw_circles3():
     print('--------------------------------------------------')
     print('Running draw_circles3:  See graphics window')
     print('--------------------------------------------------')
+    window = rg.RoseWindow(300, 300)
+    center = rg.Point(200, 150)
+    for k in range(100):
+        circle = rg.Circle(center, k+1)
+        circle.attach_to(window)
+        window.render(0.01)
+
+    window.close_on_mouse_click()
 
 
 def print_cosines():
@@ -200,7 +210,7 @@ def print_cosines():
        68.9855097830147
     """
     # ------------------------------------------------------------------
-    # TODO: 8. Implement this function, per its doc-string above.
+    # DONEN: 8. Implement this function, per its doc-string above.
     # Put a statement in  main  to test this function.
     # REQUIREMENT: You must use a   RANGE  statement to solve this problem.
     #
@@ -215,6 +225,8 @@ def print_cosines():
     print('--------------------------------------------------')
     print('Running print_cosines:')
     print('--------------------------------------------------')
+    for k in range(101):
+        print(80*math.cos(k))
 
 
 def draw_cosines_and_sines():
