18c3cf45827aa76aaac1514d4c032284fd754158
568289315 <wangl3@rose-hulman.edu>
2017-09-06 15:25:16 -0400

    Finish TODO

--- src/m6_summing.py
+++ src/m6_summing.py
@@ -70,7 +70,7 @@ def sum_cosines(n):
 def test_sum_square_roots():
     """ Tests the   sum_square_roots   function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # DONE: 4. Implement this function.
     #   It TESTS the  sum_square_roots  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -81,6 +81,18 @@ def test_sum_square_roots():
     print('--------------------------------------------------')
     print('Testing the   sum_square_roots   function:')
     print('--------------------------------------------------')
+    expected = 23.06017
+    answer = sum_square_roots(8)
+    print('Test 1 expected:',expected)
+    print('         actual:',answer)
+    expected = 41.36434
+    answer = sum_square_roots(12)
+    print('Test 2 expected:', expected)
+    print('         actual:', answer)
+    expected = 8.69213
+    answer = sum_square_roots(4)
+    print('Test 3 expected:',expected)
+    print('         actual:',answer)
 
 
 def sum_square_roots(n):
@@ -97,13 +109,17 @@ def sum_square_roots(n):
       which is about 11.854408.
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
     #   No fair running the code of  sum_square_roots  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # ------------------------------------------------------------------
+    total = 0
+    for k in range(n+1):
+        total = total + math.sqrt(2*k)
+    return total
 
 
 # ----------------------------------------------------------------------
