5c5c3d5fabe2b6268846857c581cf929ba523d56
568289315 <wangl3@rose-hulman.edu>
2017-09-06 17:01:28 -0400

    Finish TODO

--- src/m8_loops_and_objects.py
+++ src/m8_loops_and_objects.py
@@ -19,6 +19,7 @@ def main():
     draw_circles1()
     print_sequence2()
     draw_circles2()
+    print_sequence3()
 
 
 def print_sequence1():
@@ -149,7 +150,7 @@ def print_sequence3():
       100.
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement this function, per its doc-string above.
+    # DONE: 6. Implement this function, per its doc-string above.
     # Put a statement in  main  to test this function.
     # REQUIREMENT: You must use a   RANGE  statement to solve this problem.
     # ------------------------------------------------------------------
@@ -157,6 +158,8 @@ def print_sequence3():
     print('--------------------------------------------------')
     print('Running print_sequence3:')
     print('--------------------------------------------------')
+    for k in range(100):
+        print(k+1)
 
 
 def draw_circles3():
